532e63cc54d71212aed5370fee53fe54713c225b
Joyce Wu <joycewu@Joyces-MBP-2.wireless.yale.internal>
2022-12-20 22:19:15 -0500

    finalized hill climbing

--- test_cribbage.py
+++ test_cribbage.py
@@ -3,7 +3,6 @@ import sys
 from policy import CompositePolicy, RandomThrower, RandomPegger, GreedyThrower, GreedyPegger
 from cribbage import Game, evaluate_policies
 from heuristic_policy import HeuristicPolicy
-from hill_evaluate import HillEvaluate
 from genetic_evaluate import GeneticEvaluate
 
 def define_agent(flag):
@@ -12,7 +11,7 @@ def define_agent(flag):
     if flag == '--greedy':
         params = [0.5, 0.5, 0.5, 0.5, 0.5]
     if flag == '--hill':
-        params = [0.625, 0.5, 0.5, 0.5, 0.5] # TO REPLACE
+        params = [0.625, 0.625, 0.5625, 0.625, 0.5]
     if flag == '--genetic':
         genetic = GeneticEvaluate()
         params = genetic.genetic()
@@ -20,14 +19,12 @@ def define_agent(flag):
     return HeuristicPolicy(game, params)
 
 if __name__ == "__main__":
-    hill = HillEvaluate()
-    hill.hill_climb()
     game = Game()
 
-    # new_agent = define_agent(sys.argv[1])
-    # baseline = define_agent(sys.argv[2])
-    # games = int(sys.argv[3])
+    new_agent = define_agent(sys.argv[1])
+    baseline = define_agent(sys.argv[2])
+    games = int(sys.argv[3])
     
-    # results = evaluate_policies(game, new_agent, baseline, games)
-    # print("NET:", results[0])
-    # print(results)
+    results = evaluate_policies(game, new_agent, baseline, games)
+    print("NET:", results[0])
+    print(results)
